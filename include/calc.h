289a903ecc590eb04c34ddff059f15411bd82b08
Bayazit 578 <rakh.mat@inbox.ru>
2026-04-13 19:32:30 +0300

    Added intrinsic-like funcs in array mode calculation

--- include/calc.h
+++ include/calc.h
@@ -8,16 +8,6 @@
 
 #include "draw.h"
 
-const float R2MAX       = 4.f;
-const uint16_t NMAX     = 255;
-
-const int IMG_WDTH      = 800;
-const int IMG_HGHT      = 600;
-
-const __m256 R2MAX_VECT = _mm256_set1_ps(4.f);
-const __m256 NMAX_VECT  = _mm256_set1_ps(255.f);
-const __m256 TWO_VECT   = _mm256_set1_ps(2.f);
-
 typedef struct {
         float scale;
         float vert_offset;
@@ -25,14 +15,39 @@ typedef struct {
 } trans_t;
 
 typedef struct {
-        float data[8];
+    float data[8];
 } m256;
+
+typedef struct {
+    uint32_t data[8];
+} m256i;
+
+const float R2MAX   = 4.f;
+const uint16_t NMAX = 255;
+
+const int IMG_WDTH = 800;
+const int IMG_HGHT = 600;
+
+const __m256 R2MAX_VECT = _mm256_set1_ps(R2MAX);
+const __m256 TWO_VECT   = _mm256_set1_ps(2.f);
 
 void calc_mandelbrot_scalar(sfImage* image, trans_t transform,
                             color_t color_offset);
 
 void calc_mandelbrot_array(sfImage* image, trans_t transform,
                             color_t color_offset);
+
+inline __attribute__((always_inline)) m256 mm256_set1_ps_array(float val);
+inline __attribute__((always_inline)) m256 mm256_set_ps_array(float e7, float e6, float e5, float e4,
+                                                              float e3, float e2, float e1, float e0);
+inline __attribute__((always_inline)) m256 mm256_add_ps_array(m256 a, m256 b);
+inline __attribute__((always_inline)) m256 mm256_sub_ps_array(m256 a, m256 b);
+inline __attribute__((always_inline)) m256 mm256_mul_ps_array(m256 a, m256 b);
+inline __attribute__((always_inline)) m256i mm256_cmp_ps_array(m256 a, m256 b, int cmp_type);
+inline __attribute__((always_inline)) int mm256_movemask_ps_array(m256i mask);
+inline __attribute__((always_inline)) m256 mm256_blendv_ps_array(m256 a, m256 b, m256i mask);
+inline __attribute__((always_inline)) void mm256_storeu_ps_array(float* ptr, m256 a);
+inline __attribute__((always_inline)) m256 mm256_cvtepi32_ps_array(m256i a); 
 
 void calc_mandelbrot_intrin(sfImage* image, trans_t transform,
                             color_t color_offset);
--- src/calc.cpp
+++ src/calc.cpp
@@ -34,82 +34,129 @@ void calc_mandelbrot_scalar(sfImage* image, trans_t transform,
     }
 }
 
+m256 mm256_set1_ps_array(float val) {
+    m256 result;
+    for (int i = 0; i < 8; i++) {
+        result.data[i] = val;
+    }
+    return result;
+}
+
+m256 mm256_set_ps_array(float e7, float e6, float e5, float e4,
+                        float e3, float e2, float e1, float e0) {
+    m256 result = {{e0, e1, e2, e3, e4, e5, e6, e7}};
+    return result;
+}
+
+m256 mm256_add_ps_array(m256 a, m256 b) {
+    m256 result;
+    for (int i = 0; i < 8; i++) {
+        result.data[i] = a.data[i] + b.data[i];
+    }
+    return result;
+}
+
+m256 mm256_sub_ps_array(m256 a, m256 b) {
+    m256 result;
+    for (int i = 0; i < 8; i++) {
+        result.data[i] = a.data[i] - b.data[i];
+    }
+    return result;
+}
+
+m256 mm256_mul_ps_array(m256 a, m256 b) {
+    m256 result;
+    for (int i = 0; i < 8; i++) {
+        result.data[i] = a.data[i] * b.data[i];
+    }
+    return result;
+}
+
+m256i mm256_cmp_ps_array(m256 a, m256 b, int cmp_type) {
+    m256i result;
+    for (int i = 0; i < 8; i++) {
+        result.data[i] = (a.data[i] < b.data[i]) ? ~0u : 0;
+    }
+    return result;
+}
+
+int mm256_movemask_ps_array(m256i mask) {
+    int result = 0;
+    for (int i = 0; i < 8; i++) {
+        if (mask.data[i]) result |= (1 << i);
+    }
+    return result;
+}
+
+m256 mm256_blendv_ps_array(m256 a, m256 b, m256i mask) {
+    m256 result;
+    for (int i = 0; i < 8; i++) {
+        result.data[i] = mask.data[i] ? b.data[i] : a.data[i];
+    }
+    return result;
+}
+
+void mm256_storeu_ps_array(float* ptr, m256 a) {
+    for (int i = 0; i < 8; i++) {
+        ptr[i] = a.data[i];
+    }
+}
+
+m256 mm256_cvtepi32_ps_array(m256i a) {
+    m256 result;
+    for (int i = 0; i < 8; i++) {
+        result.data[i] = (float)a.data[i];
+    }
+    return result;
+}
+
 void calc_mandelbrot_array(sfImage* image, trans_t transform,
-                            color_t color_offset) {
-    float x0[8] = {};
-    float y0 = 0;
-    float indices[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+                                       color_t color_offset) {
+    m256 indices = mm256_set_ps_array(7.f, 6.f, 5.f, 4.f, 3.f, 2.f, 1.f, 0.f);
+    m256 hor_offset_vec = mm256_set1_ps_array(transform.hor_offset);
+    m256 scale_vec = mm256_set1_ps_array(transform.scale);
+    
     for (uint32_t iy = 0; iy < IMG_HGHT; iy++) {
-        y0 = (iy - transform.vert_offset) * transform.scale;
-
+        float y0 = (iy - transform.vert_offset) * transform.scale;
+        m256 y0_vec = mm256_set1_ps_array(y0);
+        
         for (uint32_t ix = 0; ix < IMG_WDTH; ix += 8) {
-            for (uint32_t i = 0; i < 8; i++) {
-                x0[i] = ix + indices[i];
-            }
-
-            for (uint32_t i = 0; i < 8; i++) {
-                x0[i] = x0[i] - transform.hor_offset;
-            }
-
-            for (uint32_t i = 0; i < 8; i++) {
-                x0[i] = x0[i] * transform.scale;
-            }
-
-            float x[8] = {};
-            float y[8] = {};
-            uint32_t N[8] = {};
-            uint32_t any_active = 0;
+            m256 ix_vec = mm256_set1_ps_array((float)ix);
+            
+            m256 x0_vec = mm256_add_ps_array(ix_vec, indices);
+            x0_vec = mm256_sub_ps_array(x0_vec, hor_offset_vec);
+            x0_vec = mm256_mul_ps_array(x0_vec, scale_vec);
+            
+            m256 x_vec = mm256_set1_ps_array(0);
+            m256 y_vec = mm256_set1_ps_array(0);
+            m256 N_vec = mm256_set1_ps_array(0);  // N_vec теперь m256 (float)
+            m256 TWO_VECT = mm256_set1_ps_array(2.0f);
+            m256 R2MAX_VECT = mm256_set1_ps_array(R2MAX);
             
             for (uint32_t iter = 0; iter < NMAX; iter++) {
-                float x2[8] = {},
-                      y2[8] = {}, 
-                      xy[8] = {}, 
-                      r2[8] = {};
-                uint32_t cmp_mask[8] = {};
-                any_active = 0;
+                m256 x2_vec = mm256_mul_ps_array(x_vec, x_vec);
+                m256 y2_vec = mm256_mul_ps_array(y_vec, y_vec);
+                m256 xy_vec = mm256_mul_ps_array(x_vec, y_vec);
+                m256 r2_vec = mm256_add_ps_array(x2_vec, y2_vec);
                 
-                for (uint32_t i = 0; i < 8; i++) {
-                    x2[i] = x[i] * x[i];
-                }
-
-                for (uint32_t i = 0; i < 8; i++) {
-                    y2[i] = y[i] * y[i];
-                }
-
-                for (uint32_t i = 0; i < 8; i++) {
-                    xy[i] = x[i] * y[i];
-                }
-
-                for (uint32_t i = 0; i < 8; i++) {
-                    r2[i] = x2[i] + y2[i];
-                }
-
-                for (uint32_t i = 0; i < 8; i++) {
-                    x[i] = x2[i] - y2[i] + x0[i];
-                }
+                x_vec = mm256_add_ps_array(mm256_sub_ps_array(x2_vec, y2_vec), x0_vec);
+                y_vec = mm256_add_ps_array(mm256_mul_ps_array(TWO_VECT, xy_vec), y0_vec);
                 
-                for (uint32_t i = 0; i < 8; i++) {
-                    y[i] = 2.0f * xy[i] + y0;
-                }
+                m256 new_N_vec = mm256_add_ps_array(N_vec, mm256_set1_ps_array(1.0f));
+                m256i cmp_mask_vec = mm256_cmp_ps_array(r2_vec, R2MAX_VECT, 0);
                 
-                for (uint32_t i = 0; i < 8; i++) {
-                    cmp_mask[i] = (r2[i] < R2MAX) ? ~0u : 0;
-                }
-
-                for (uint32_t i = 0; i < 8; i++) {
-                    N[i] = (cmp_mask[i]) ? N[i] + 1 : N[i];
-                }
+                N_vec = mm256_blendv_ps_array(N_vec, new_N_vec, cmp_mask_vec);
                 
-                for (uint32_t i = 0; i < 8; i++) {
-                    any_active |= cmp_mask[i];
-                }
-
-                if (!any_active) break;
+                int mask = mm256_movemask_ps_array(cmp_mask_vec);
+                if (mask == 0) break;
             }
-
+            
+            float N[8];
+            mm256_storeu_ps_array(N, N_vec);
+            
             for (uint32_t i = 0; i < 8; i++) {
                 sfImage_setPixel(image, (sfVector2u){ix + i, iy}, 
-                                 set_color_by_N(N[i], color_offset));
+                                 set_color_by_N((uint32_t)N[i], color_offset));
             }
         }
     }
@@ -151,7 +198,7 @@ void calc_mandelbrot_intrin(sfImage* image, trans_t transform,
                 __m256 new_N_vec = _mm256_add_ps(N_vec, _mm256_set1_ps(1));
 
                 cmp_mask_vec = _mm256_cmp_ps(r2_vec, R2MAX_VECT, _CMP_LT_OQ);
-                N_vec = _mm256_blendv_ps(N_vec, new_N_vec, cmp_mask_vec); 
+                N_vec = _mm256_blendv_ps(N_vec, new_N_vec, cmp_mask_vec);
 
                 int mask = _mm256_movemask_ps(cmp_mask_vec);
 
